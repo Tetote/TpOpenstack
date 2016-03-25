6461e5d85d102497768064e0d8c6c59c2b5cdfcc
Tetote <theo.vaucher@gmail.com>
2016-03-25 16:49:48 +0100

    NO ERROR MY FRIEND

--- src/Client.java
+++ src/Client.java
@@ -1,52 +1,54 @@
 
-  import java.net.MalformedURLException;
-  import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.InvalidParameterException;
 
 import org.apache.xmlrpc.XmlRpcException;
-  import org.apache.xmlrpc.client.XmlRpcClient;
-  import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
-  import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
-  import org.apache.xmlrpc.client.util.ClientFactory;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
+import org.apache.xmlrpc.client.util.ClientFactory;
 //  import org.apache.xmlrpc.demo.proxy.Adder;
 
-  public class Client {
-      public static void main(String[] args) throws Exception {
-    	  
-    	  if (args.length < 3 || args.length > 3) {
-    		  throw new InvalidParameterException();
-    	  }
-    	  
-    	  int nbRequestSec = Integer.parseInt(args[0]);
-    	  String ipRepartiteur = args[1];
-    	  String portRepartiteur = args[2];
-    	  
-          // create configuration
-          XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-          config.setServerURL(new URL("http://127.0.0.1:8080/xmlrpc"));
-          config.setEnabledForExtensions(true);  
-          config.setConnectionTimeout(60 * 1000);
-          config.setReplyTimeout(60 * 1000);
+public class Client {
+	public static void main(String[] args) throws Exception {
 
-          XmlRpcClient client = new XmlRpcClient();
-        
-          // use Commons HttpClient as transport
-          client.setTransportFactory(
-              new XmlRpcCommonsTransportFactory(client));
-          // set configuration
-          client.setConfig(config);
+		int nbRequete = 18;
+		String nodeLocation = "127.0.0.1";
+		int port = 8080;
 
-          // make the a regular call
-          Object[] params = new Object[]
-              { new Integer(2), new Integer(3) };
-          Integer result = (Integer) client.execute("Calculator.add", params);
-          System.out.println("2 + 3 = " + result);
-        
-          // make a call using dynamic proxy
-	  /*          ClientFactory factory = new ClientFactory(client);
+		if (args.length == 3) {
+			nbRequete = Integer.parseInt(args[0]);
+			nodeLocation = args[1];
+			port = Integer.parseInt(args[2]);
+		}
+
+		// create configuration
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+		config.setServerURL(new URL("http://"+nodeLocation+":"+port+"/xmlrpc"));
+		config.setEnabledForExtensions(true);  
+		config.setConnectionTimeout(60 * 1000);
+		config.setReplyTimeout(60 * 1000);
+
+		XmlRpcClient client = new XmlRpcClient();
+
+		// use Commons HttpClient as transport
+		client.setTransportFactory(
+				new XmlRpcCommonsTransportFactory(client));
+		// set configuration
+		client.setConfig(config);
+
+		// make the a regular call
+		Object[] params = new Object[]
+				{ new Integer(2), new Integer(3) };
+		Integer result = (Integer) client.execute("Calculator.add", params);
+		System.out.println("2 + 3 = " + result);
+
+		// make a call using dynamic proxy
+		/*          ClientFactory factory = new ClientFactory(client);
           Adder adder = (Adder) factory.newInstance(Adder.class);
           int sum = adder.add(2, 4);
           System.out.println("2 + 4 = " + sum);
-	  */
-      }
-  }
+		 */
+	}
+}
